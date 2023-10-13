70cfc073e82eead752c8e7d7d86ee1fa1eea7f37
Karma <michele.carmagnani@gmail.com>
2023-10-13 13:36:57 +0200

    feat: login

--- frontend/app/api/login/route.ts
+++ frontend/app/api/login/route.ts
@@ -7,7 +7,7 @@ type loginPros = {
 
 export async function POST(request: Request) {
   const { email, password }: loginPros = await request.json();
-  const res = await fetch(`${process.env.BASE_URL}/Accaunts/login`, {
+  const res = await fetch(`http://localhost:3000/Accaunts/login`, {
     method: "POST",
     body: JSON.stringify({ userNbame: email, password }),
     headers: {
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
   });
 
   const user = res.json();
-  
+
   if (!user) return NextResponse.error();
 
   return NextResponse.json(user);
