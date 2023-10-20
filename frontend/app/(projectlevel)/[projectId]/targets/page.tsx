8f08b30cfc8ff1b9d29c3598a300d06b179f7501
Karma <michele.carmagnani@gmail.com>
2023-10-20 17:14:11 +0200

    feat: delete project

--- frontend/app/(projectlevel)/[projectId]/targets/page.tsx
+++ frontend/app/(projectlevel)/[projectId]/targets/page.tsx
@@ -7,11 +7,17 @@ import { columns } from "./table/columns"
 import useTargets from "@/hooks/useTargets"
 import AddTargetDialog from "./dialogs/add-target-dialog"
 import { useParams } from "next/navigation"
+import useProjects from "@/hooks/useProjects"
+import EmptyState from "@/components/emptly-state"
 
 export default function TargetsPage() {
   const { projectId }: { projectId: string } = useParams()
   const { targets } = useTargets(projectId)
+  const { projects } = useProjects()
 
+  const project = projects?.items?.find(
+    (item) => item.id === parseInt(projectId) || { id: -1 }
+  )
   if (!targets?.items) return null
 
   return (
